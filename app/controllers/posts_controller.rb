e8cd186ee30099506d7d216c1481768b580aadeb
github username <github email address>
2018-06-11 15:00:04 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
 class PostsController < ApplicationController
 
   def new
-    @post = Post.new(author_id: params[:author_id])
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = Post.new(author_id: params[:author_id])
+    end
   end
     
   def index
-    binding.pry
     if params[:author_id]
       @posts = Author.find(params[:author_id]).posts
     else
@@ -34,7 +37,19 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
+    # author = Author.find_by(id: params[:id])
+    if params[:author_id]
+      author = Author.find_by(id: params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+      else
+        @post = author.posts.find_by(id: params[:id])
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      end
+    else
+      @post = Post.find(params[:id])
+    end
   end
 
   private
