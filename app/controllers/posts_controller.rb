4e334f980dd87f3a12817cfc98cc1ab50e007571
github username <github email address>
2018-06-11 13:24:29 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
 
+  def new
+    @post = Post.new(author_id: params[:author_id])
+  end
+    
   def index
+    binding.pry
     if params[:author_id]
       @posts = Author.find(params[:author_id]).posts
     else
@@ -14,10 +19,6 @@ class PostsController < ApplicationController
     else
       @post = Post.find(params[:id])
     end
-  end
-
-  def new
-    @post = Post.new
   end
 
   def create
@@ -39,6 +40,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
